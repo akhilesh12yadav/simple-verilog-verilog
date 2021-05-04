01c513c8b70072e079fa56b4d35dc66f240f6a47
Akhilesh Yadav <55866802+akhilesh12yadav@users.noreply.github.com>
2021-05-04 11:32:58 +0530

    Add files via upload

---
+++ D_FF.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+
+module Dff(
+
+    input wire clk ,
+    input wire clr,
+    input wire D,
+    output reg q
+    );
+
+always @(posedge clk or posedge clr)
+		if (clr == 1)
+				q <= 0;
+		else 
+				q<= D;
+
+endmodule
+
---
+++ Dff_tb.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+
+`include "D_FF.v"
+
+module DFF_tb;
+
+	// Inputs
+	reg clk;
+	reg clr;
+	reg D;
+
+	// Outputs
+	wire q;
+
+	// Instantiate the Unit Under Test (UUT)
+	Dff uut (clk,clr,D,q);
+
+always #20 clk = ~clk;
+
+initial begin
+        #2000 $finish; // Finsh execution
+end
+
+	initial begin
+               $dumpfile("DFF_tb.vcd");
+               $dumpvars(0, DFF_tb);
+		
+                // Initialize Inputs
+		clk = 0;
+		clr = 1;
+		D = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+		clr =0;
+		D=1;
+		#40 ;
+		D = 0;
+                #40
+                D = 1;
+               $monitor($time," D= %b",D);
+               $display ("finish");
+	end
+      
+endmodule
+
